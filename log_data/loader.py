7f6eb7e7f2e8894b317fbda2f90359755e30600c
Seung Min Lee <profile2697@nethru.co.kr>
2017-10-12 14:28:34 +0900

    modified loader not to use id list

--- log_data/loader.py
+++ log_data/loader.py
@@ -1,8 +1,6 @@
 import os
 import pandas as pd
 
-batch_ids = ['20171001', '20171002', '20171003', '20171004', '20171005', '20171006', '20171007', '20171008']
-
 
 class LogDataLoader:
     def __init__(self, log_dir='logs'):
@@ -12,11 +10,8 @@ class LogDataLoader:
         """
         log_dir 아래에 있는 log 데이터를 읽어 pandas dataframe으로 만든다.
 
-        :param batch_number: [1, 8] 범위의 숫자, 기본값은 1
+        :param batch_number: 1이상의 자연수. 기본값은 1
         :return: 해당 batch를 읽어 pandas의 dataframe을 반환
         """
-        batch_id = '2017100' + str(batch_number)
-        if batch_id not in batch_ids:
-            raise ValueError("batch_number는 1~8 중 하나의 자연수여야 합니다.")
-
+        batch_id = '201710' + str(batch_number).zfill(2)
         return pd.read_csv(os.path.join(self.log_dir, 'convertlog.' + batch_id), sep='\t')
