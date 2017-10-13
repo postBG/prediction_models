4d1b207a00106639c7d37c21e00303842b50f531
Seung Min Lee <profile2697@nethru.co.kr>
2017-10-13 10:58:11 +0900

    날짜를 커맨드 입력으로 받을 수 있도록 수정

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from sklearn.ensemble import RandomForestClassifier
 
 from log_data.loader import LogDataLoader
@@ -5,9 +6,13 @@ from log_data.processor import split_data, normalize_feature, one_hot_encoder, t
 from models.sklearn import SklearnHelper
 from printers.plt import print_confusion_matrix
 
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_integer('date', 10,
+                            """테스트에 사용할 날짜""")
+
 if __name__ == '__main__':
     loader = LogDataLoader()
-    batch = loader.load_batch(10)
+    batch = loader.load_batch(FLAGS.date)
     batch = transform_pay_as_label(batch)
     batch = normalize_feature(batch,
                               ['duration(sec)', 'visit', 'event', 'pv', 'productview', 'cart', 'wishlist', 'order'])
