5f8ce0eef439e068e4a93c73d4a82b6fb7921ece
Seung Min Lee <profile2697@nethru.co.kr>
2017-10-13 18:41:49 +0900

    add major metrics to plt

--- printers/plt.py
+++ printers/plt.py
@@ -1,3 +1,5 @@
+import random
+
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
@@ -29,11 +31,32 @@ def print_confusion_matrix(labels, predicted_labels, class_names=(0, 1)):
     plt.xticks(range(width), class_names[:width], rotation=30)
     plt.yticks(range(height), class_names[:height])
 
+    precision, recall, f1_score, accuracy = get_metrics(cm)
+
+    plt.figtext(0.01, 0.13, "Precision: {:2f}".format(precision))
+    plt.figtext(0.01, 0.09, "Recall: {:2f}".format(recall))
+    plt.figtext(0.01, 0.05, "F1-score: {:2f}".format(f1_score))
+    plt.figtext(0.01, 0.01, "Accuracy: {:2f}".format(accuracy))
+
     plt.show()
 
+
+def get_metrics(cm):
+    precision = cm[1, 1] / sum(cm[:, 1])
+    recall = cm[1, 1] / sum(cm[1, :])
+    f1_score = 2 * precision * recall / (precision + recall)
+    accuracy = (cm[0, 0] + cm[1, 1]) / sum(sum(cm))
+
+    return precision, recall, f1_score, accuracy
 
 def get_font_color(value):
     if value < 5:
         return "black"
     else:
         return "white"
+
+
+if __name__ == '__main__':
+    true_labels = [random.randint(1, 10) for i in range(1)]
+    predicted = [random.randint(1, 10) for i in range(1)]
+    print_confusion_matrix(true_labels, predicted)
