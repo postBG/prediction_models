7224988ef4566ca7c3380711e8f612927d62ad3b
Seung Min Lee <profile2697@nethru.co.kr>
2017-10-18 16:47:48 +0900

    refactoring main

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_integer('date', 10,
                             """테스트에 사용할 날짜""")
 
-if __name__ == '__main__':
+
+def main():
     loader = LogDataLoader()
     batch = loader.load_batch(FLAGS.date)
     batch = DefaultPreprocessor().process(batch)
@@ -31,3 +32,7 @@ if __name__ == '__main__':
 
     predicted_labels = rf.predict(test_data.drop('pay', axis=1))
     print_confusion_matrix(test_data.pay.apply(lambda pay: 1 if pay > 0 else 0), predicted_labels)
+
+
+if __name__ == '__main__':
+    main()
